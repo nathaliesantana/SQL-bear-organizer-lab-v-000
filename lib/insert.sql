47b91436684b1fa85c17eb421d7a4323b3df36f8
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-13 04:35:26 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color) VALUES ("Mr. Chocolate", 2, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Rowdy", 2, "Black");
+INSERT INTO bears (name, age, color) VALUES ("Tabitha", 2, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Sergeant Brown", 2, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Melissa", 2, "Black");
+INSERT INTO bears (name, age, color) VALUES ("Grinch", 2, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Wendy", 2, "Brown");
+INSERT INTO bears (name, age, color) VALUES (NULL, NULL, "Black");
